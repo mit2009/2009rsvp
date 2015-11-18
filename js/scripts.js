5b5a8363caf58aa0f953c70d8a4af450bf224fbb
poofytoo <victorhung92@gmail.com>
2015-11-17 22:20:02 -0500

    main page, font and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,89 @@
+function drawTopTrapezoid() {
+  ww = $(window).width();
+  points = '0,0 ' + 
+    ww + ',0 ' +
+    ww + ',70 ' + 
+    '0,110';
+  $('.slanted-background-top').find('polygon').attr('points', points)
+}
+
+function drawBottomTrapezoid() {
+  ww = $(window).width();
+  points = '0,0 ' + 
+    ww + ',50 ' +
+    ww + ',110 ' + 
+    '0,110';
+  $('.slanted-background-bottom').find('polygon').attr('points', points)
+}
+
+function init() {
+  drawTopTrapezoid();
+  drawBottomTrapezoid();
+}
+
 $(function() {
+
+  $(window).resize(function() {
+    drawTopTrapezoid();
+    drawBottomTrapezoid();
+  });
+
+  init()
+
   var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies;
 
-  // create a Matter.js engine
   var engine = Engine.create(document.body, { 
     render: {
-        options: {
-            wireframes: false
-        }
-    }
-});
-
-  // create two boxes and a ground
-  var boxA = Bodies.circle(400, 200, 20, { restitution: 0.9} );
-  var boxB = Bodies.circle(450, 50, 20, { restitution: 0.9,    
-    render: {
-         fillStyle: 'red',
-         lineStyle: 'none'
+      fillStyle: 'none',
+      options: {
+        wireframes: false
+      }
     }
   });
 
-  var ground = Bodies.rectangle(400, 610, 810, 60, { restitution: 0.9, isStatic: true, angle: Math.PI * 0.04  });
+  colouredBalls = ['red', 'blue', 'gray', 'yellow', 'orange', 'green', 'purple', 'pink']
+  ballBodies = []
+  for (i = 0; i < 8; i ++) {
+    ball = Bodies.circle(i*30 + 50, 100, 10,
+    {
+      friction: 0,
+      restitution: 1.1,
+      render: {
+        fillStyle: colouredBalls[i],
+        strokeStyle: colouredBalls[i]
+      }
+    })
+    Matter.Body.applyForce(ball, {
+      x : 0,
+      y : 0
+    }, {
+      x : (Math.random()-0.5)/400,
+      y : -Math.random()/300
+    })
+    ballBodies.push(ball)
+  }
 
-  // add all of the bodies to the world
-  World.add(engine.world, [boxA, boxB, ground]);
+  var ground = Bodies.rectangle(400, 610, 810, 60, { 
+    restitution: 1.6, 
+    friction: 0, 
+    isStatic: true, 
+    angle: Math.PI * 0.04  
+  });
 
-  // run the engine
+  ballBodies.push(ground)
+  World.add(engine.world, ballBodies);
   Engine.run(engine);
-
+  engine.world.gravity.y = 1
 })
 
 if (window.DeviceOrientationEvent) {
-  console.log('tilty')
-  // Listen for the deviceorientation event and handle the raw data
   window.addEventListener('deviceorientation', function(eventData) {
-    // gamma is the left-to-right tilt in degrees, where right is positive
     var tiltLR = eventData.gamma;
-
-    // beta is the front-to-back tilt in degrees, where front is positive
     var tiltFB = eventData.beta;
-
-    // alpha is the compass direction the device is facing in degrees
     var dir = eventData.alpha
-
-    // call our orientation event handler
     console.log(tiltLR, tiltFB, dir);
-    // deviceOrientationHandler(tiltLR, tiltFB, dir);
   }, false);
 
 } else {
